de43a63425ca869e2c090291fef21146a3c36bb6
Kelvin Li <jpkelvinli@gmail.com>
2019-03-01 19:00:33 -0800

    Allow users to select metrics and filter links in communication views.

--- src/components/CommPanels.js
+++ src/components/CommPanels.js
@@ -12,7 +12,7 @@ export default {
   },
   data: () => ({
     socketError: false,
-    server: 'localhost:8888',
+    server: 'vidi1.cs.ucdavis.edu:8888',
     timeDomains: ['LastGvt','RealTs','VirtualTime'],
     granularity: ['PE','KP','LP'],
     measures: ['avg', 'sum', 'max', 'min'],
@@ -22,32 +22,32 @@ export default {
     selectedMeasure: 'sum',
     metrics: [
       'RbSec',
-      'RbTotal'
+      'RbPrim'
     ],
-    timeIntervals: [],
+    timeIntervals: [
+      [204471, 238514],
+      [242484, 263715],
+      [280495, 363079],
+      // [365019, 366747],
+    ],
+    processIds: [],
+    clusterIds: [],
+    clusterColors: [
+      [227, 119, 194],
+      [188, 189, 34],
+      [23, 190, 207],
+      [127, 127, 127]],
   }),
-  mounted() {
+  mounted () {
     this.init()
   },
 
   methods: {
-    init() {
+    init () {
       let socket = new WebSocket('ws://' + this.server + '/websocket')
       socket.onopen = () => {
         this.dialog = !this.dialog
         this.socketError = false
-        let testParams = {
-            "timeIntervals": [
-                [204471, 238514],
-                [242484, 263715],
-                [280495, 363079],
-                [365019, 366747],
-                [380636, 393968]
-            ],
-            "ringMetrics": ["RbSec"]
-        }
-        socket.send(JSON.stringify({method: 'set', params: testParams}))
-
         socket.send(JSON.stringify({method: 'get', data: 'KpData'}))
       }
 
@@ -57,26 +57,46 @@ export default {
   
       socket.onmessage = (event) => {
         let data = JSON.parse(event.data)
+        data.data.forEach(d => {
+          d.RbPrim = d.RbTotal - d.RbSec
+        })
         this.data = data.data
         if (data.schema.hasOwnProperty('CommData')) {
           data.schema.CommData = 'int'
         }
-        let params = data.params
-        this.timeIntervals = params.timeIntervals
+        this.$refs.container.allMetrics = Object.keys(data.schema).filter(k => k.slice(-2) !== 'id' && k.slice(-2) !== 'Id')
+        let params = data.params || {}
+
+        if(params.timeIntervals) this.timeIntervals = params.timeIntervals
         if(params.timeMetric) this.timeDomain = params.timeMetric
-        if(params.ringMetrics) this.metrics = params.ringMetrics
+        if(params.ringMetrics) {
+          this.metrics = params.ringMetrics
+          this.$refs.container.metrics = params.ringMetrics
+        }
+        if(params.processIds) this.processIds = params.processIds
+        if(params.clusterIds) this.clusterIds = params.clusterIds
+        if(params.clusterColors) this.clusterColors = params.clusterColors.map(c => 'rgb(' + c.join(',') + ')')
         this.update()
       }
 
     },
-    update() {
+    update () {
       this.$refs.container.visualize({
         data: this.data,
         measure: 'sum',
         timeDomain: this.timeDomain,
         metrics: this.metrics,
-        timeIntervals: this.timeIntervals
+        timeIntervals: this.timeIntervals,
+        processIds: this.processIds,
+        clusterIds: this.clusterIds,
+        clusterColors: this.clusterColors
       })
+    },
+
+    updateMetrics (metrics) {
+      this.metrics = metrics
+      this.update()
     }
+
   }
 }
--- src/components/Communication.js
+++ src/components/Communication.js
@@ -6,14 +6,19 @@ export default {
   name: 'Communication',
   template,
   data: () => ({
+    drawer: false,
     data: null,
     timeDomain: null,
+    allMetrics: [],
     metrics: [],
     showIntraComm: false,
     numKP: 0,
     numPE: 0,
     threshold: 0,
-    maxLinkValue: 1
+    rings: [],
+    maxLinkValue: 1,
+    isComparisonMode: false,
+    granularity: 'KpGid'
   }),
   computed: {
     thresholdValue: function() {
@@ -21,23 +26,42 @@ export default {
     }
   },
   methods: {
+    updateLink () {
+      this.rings.forEach(ring => {
+        ring.updateLink(this.thresholdValue )
+      })
+    },
+
+    updateMetrics () {
+      this.$emit('updateMetrics', this.metrics)
+    },
+
     visualize({
       data,
       timeDomain,
       metrics,
       timeIntervals,
-      measure
+      measure,
+      processIds = [],
+      clusterIds = [],
+      clusterColors = ['red', 'blue', 'yellow', 'green', 'purple']
     }) {
       if(data !== undefined && Array.isArray(data)) {
         this.data = data
-        this.numPE = Math.max(...data.map(d => d.Peid)) + 1
-        this.numKP = Math.max(...data.map(d => d.Kpid)) + 1
+        if (processIds.length > 0) {
+          this.data = data.filter(d => processIds.indexOf(d.KpGid))
+        }
+        this.numPE = Math.max(...this.data.map(d => d.Peid)) + 1
+        this.numKP = Math.max(...this.data.map(d => d.Kpid)) + 1
       }
-      console.log(this.numPE, this.numKP, timeIntervals)
+      this.rings = new Array(timeIntervals.length)
+      let colorDomains = new Array(metrics.length + 1)
       let container = this.$refs.container
       container.innerHTML = ''
-      let rings
-      let colorDomains = new Array(metrics.length + 1)
+      if (timeIntervals.length > 1) {
+        this.isComparisonMode = true
+      }
+
       timeIntervals.forEach((timeInterval, tii) => {
         let aggregateByTime = p3.pipeline()
         if(timeInterval !== null) {
@@ -45,20 +69,20 @@ export default {
           match[timeDomain] = timeInterval
           aggregateByTime.match(match)
         }
-  
+        
         let tsData = aggregateByTime.aggregate({
           $group: timeDomain,
           $collect: {
             items: {$data: '*'}
           }
         }).execute(this.data)
-  
+        
         let tsCommData = tsData.map(sample => {
           return sample.items.sort((a,b) => a.Peid - b.Peid).map(item => item.CommData)
         })
   
         let accCommData = tsCommData[0]
-        // console.log(accCommData)
+  
         if(tsCommData.length > 1) {
           tsCommData.slice(1).forEach(sample => {
             sample.forEach((rows, i) => {
@@ -77,13 +101,13 @@ export default {
         })
     
         let commData = accCommData[0].map((a, i) => p3.vector.sum(accCommData.slice(i*this.numKP, (i+1)*this.numKP)))
-  
+
         let collection = {}
         for (let metric of metrics) {
-            collection[measure + '.' + metric] = {}
-            collection[measure + '.' + metric]['$'+measure] = metric
+          collection[measure + '.' + metric] = {}
+          collection[measure + '.' + metric]['$'+measure] = metric
         }
-  
+
         let aggrSpec = {
           $aggregate: {
             $group: ['Kpid', 'Peid'],
@@ -92,18 +116,25 @@ export default {
         }
   
         let transform = p3.pipeline()
-  
-        if(timeInterval !== null) {
-          let match = {}
+        let match = {}
+        if (timeInterval !== null) {
           match[timeDomain] = timeInterval
+        }
+
+        if (Object.keys(match).length) {
           transform.match(match)
         }
-        
+
         let result = transform.aggregate(aggrSpec.$aggregate)
           .execute(this.data)
-  
-        let metricData = p3.aggregate(result, {$group: 'Peid', $collect: {items: {$data: '*'}}}).map(r => r.items)
-  
+
+        let metricData = p3.aggregate(result, {
+          $group: 'Peid',
+          $collect: {items: {$data: '*'}}
+        })
+        .sort((a, b) => a.items[0].Peid - b.items[0].Peid)
+        .map(r => r.items)
+
         let colorSets = [
           ['white', 'teal'],
           ['white', 'purple'],
@@ -118,28 +149,47 @@ export default {
           {
             type: 'link',
             data: commData,
-            threshold: 10000,
+            threshold: 0,
             ignoreDiagonal: !this.showIntraComm,
             size: 2,
-            vmap: {
-              color: 'CommData'
-            },
+            vmap: { color: 'CommData' },
             colors: ['steelblue', 'red'],
             colorDomain: colorDomains[0]
           },
         ]
-  
-        metrics.forEach( (metric, mi) => {
-          layers.push({
-            type: 'rect',
-            data: metricData,
-            vmap: {
-              color: measure + '.' + metric
-            },
-            colors: colorSets[mi],
-            size: 1,
-            colorDomain: colorDomains[mi+1]
+
+        let layerSpec = {
+          type: 'rect',
+          size: 1,
+        }
+
+        if(clusterIds.length && processIds.length) {
+          metricData.forEach( (items) => {
+            items.forEach((item)=> {
+              let gid = item.Peid * this.numKP + item.Kpid
+              let pid = processIds.indexOf(gid)
+              item.clusterId = clusterIds[pid]
+            })
           })
+
+          layerSpec.colors = function (d) {
+            return clusterColors[d]
+          }
+          layerSpec.vmap = {color: 'clusterId'}
+        }
+
+        metrics.forEach( (metric, mi) => {
+          let layer = Object.assign({}, layerSpec)
+          layer.data = metricData
+          layer.colorDomain = colorDomains[mi+1]
+
+          if(clusterIds.length && processIds.length) {
+            layer.vmap.size = measure + '.' + metric
+          } else {
+            layer.vmap = { color: measure + '.' + metric }
+            layer.colors = colorSets[mi]
+          }
+          layers.push(layer)
         })
   
         layers.push({
@@ -150,7 +200,7 @@ export default {
         
         let div = document.createElement('div')
         let radius = container.clientWidth / timeIntervals.length
-        rings = picos({
+        this.rings[tii] = picos({
           config: {
             container: div,
             legend: (tii === timeIntervals.length - 1),
@@ -161,7 +211,7 @@ export default {
         })
   
         container.appendChild(div)
-        rings.forEach( (ring, rii) => {
+        this.rings[tii].forEach( (ring, rii) => {
           if (Array.isArray(colorDomains[rii])) {
             colorDomains[rii][0] = Math.min(colorDomains[rii][0], ring.colorDomain[0])
             colorDomains[rii][1] = Math.max(colorDomains[rii][1], ring.colorDomain[1])
@@ -173,11 +223,15 @@ export default {
         })
       })
       this.maxLinkValue = colorDomains[0][1]
-      rings.forEach((ring, rii) => {
-        if(typeof(ring.updateColor) === 'function') {
-          ring.updateColor(colorDomains[rii])
-        }
-      })
+      if(timeIntervals.length > 1) {
+        this.rings.forEach(ringLayers=> {
+          ringLayers.forEach((ringLayer,rii) => {
+            if(typeof(ringLayer.updateColor) === 'function') {
+              ringLayer.updateColor(colorDomains[rii])
+            }
+          })
+        })
+      }
     }
   }
 }
